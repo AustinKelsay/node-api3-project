d16de44fcaf0884e892712028c96c7c11c3dbd3b
AustinKelsay <austinkelsay@yahoo.com>
2020-05-11 17:01:30 -0500

    Finished all postRouter CRUD operations

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,20 +2,46 @@ const express = require('express');
 
 const router = express.Router();
 
+const posts = require("./postDb")
+
 router.get('/', (req, res) => {
-  // do your magic!
+  posts
+  .get()
+  .then((users) => {
+    res.status(200).json(users)
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: "Error retrieving posts"
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  posts
+  .getById(req.params.id)
+  .then((post) => {
+    res.status(200).json(post)
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: "Error retrieving post"
+    })
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  posts
+    .remove(req.params.id)
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(500).json({ message: err }));
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  posts
+    .update(req.params.id, req.body)
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(500).json({ message: err }));
 });
 
 // custom middleware
