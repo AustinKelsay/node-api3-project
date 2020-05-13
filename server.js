b5093a5bedd80e8f304ec1c8109db0565058a8ae
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-05-12 20:32:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const postRouter = require("./posts/postRouter")
 const logger = require("./middleware/logger")
 
 const server = express();
-const port = 5000;
+const port = (process.env.PORT || 5000;
 
 //custom middleware
 
